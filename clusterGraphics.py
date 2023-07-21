fixed bug for projections outside of simplices in delaunay density calculation

clusterGraphics.py
@@ -21,13 +21,18 @@ import spCluster as cluster
 ################################################################################
 ############################ Local density analysis ############################
 ################################################################################
-def plotSimplexDensity(dirName, figureName, pad = 1, logy=False):
-    if(os.path.exists(dirName + os.sep + 'simplexDensity.dat')):
+def plotSimplexDensity(dirName, figureName, which = False, pad = 1, logy=False):
+    if(os.path.exists(dirName + os.sep + 'simplexDensity!.dat')):
         simplexDensity = np.loadtxt(dirName + os.sep + 'simplexDensity.dat')
     else:
         _, simplexDensity = cluster.computeDelaunayCluster(dirName, threshold=0.78, filter=False)
     denseSimplexList = np.loadtxt(dirName + os.sep + 'denseSimplexList.dat')
-    simplexDensity = simplexDensity[denseSimplexList==1]
+    if(which == 'fluid'):
+        simplexDensity = simplexDensity[denseSimplexList==1]
+        figureName = figureName + '-' + which
+    elif(which == 'gas'):
+        simplexDensity = simplexDensity[denseSimplexList==0]
+        figureName = figureName + '-' + which
     fig, ax = plt.subplots(1, 2, figsize=(9,4), dpi=150)
     ax[0].plot(np.arange(1, simplexDensity.shape[0]+1, 1), np.sort(simplexDensity), color='k', marker='.', markersize=8, lw=0.8, fillstyle='none')
     ax[0].tick_params(axis='both', labelsize=12)
@@ -55,7 +60,7 @@ def plotSimplexDensity(dirName, figureName, pad = 1, logy=False):
     ax[1].set_xlabel('$\\varphi^{Simplex}$', fontsize=16)
     fig.tight_layout()
     fig.subplots_adjust(wspace=0.3)
-    plt.savefig("/home/francesco/Pictures/soft/mips/simplexPDF" + figureName + ".png", transparent=True, format="png")
+    plt.savefig("/home/francesco/Pictures/soft/mips/simplexPDF-" + figureName + ".png", transparent=True, format="png")
     plt.show()
 
 def fitPhiPDF(dirName, figureName, numBins):
@@ -1170,7 +1175,121 @@ def plotSPPressureProfile(dirName, figureName, shift=0, which='pressure'):
     fig.savefig(figureName, transparent=True, format = "png")
     plt.show()
 
-def plotSPClusterDensity(dirName, figureName, fixed=False, which='1e-03'):
+def plotSPAreaVSRemoved(dirName, figureName):
+    fig, ax = plt.subplots(figsize=(7,4.5), dpi = 120)
+    Nlist = np.array(['4096', '8192', '16384'])
+    sampleList = np.array(['0.28', '0.27', '0.27'])
+    dirList = np.array(['10', '50', '100', '500', '1000', '2000', '3000', '4000'])
+    markerList = np.array(['v', 'o', 's'])
+    for n in range(Nlist.shape[0]):
+        fluidArea = []
+        gasArea = []
+        phi = []
+        initialPhi = float(utils.readFromParams(dirName + os.sep + Nlist[n] + "-2d" + "/densitySweep/" + sampleList[n] + "/active-langevin/Dr2e-04/", 'phi'))
+        for d in range(dirList.shape[0]):
+            dirSample = dirName + os.sep + Nlist[n] + "-2d" + "/densitySweep/" + sampleList[n] + "/active-langevin/Dr2e-04/" + dirList[d] + "removed/"
+            if(os.path.exists(dirSample)):
+                phi.append(float(utils.readFromParams(dirSample, 'phi')))
+                if(os.path.exists(dirSample + os.sep + "simplexArea.dat")):
+                    denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
+                    simplexDensity = np.loadtxt(dirSample + os.sep + "simplexDensity.dat")
+                    simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
+                else:
+                    _, simplexDensity = cluster.computeDelaunayCluster(dirSample, 0.78, filter=False)
+                    denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
+                    simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
+                fluidArea.append(np.sum(simplexArea[denseSimplexList==1]))
+                gasArea.append(np.sum(simplexArea[denseSimplexList==0]))
+        phi = np.array(phi)
+        fluidArea = np.array(fluidArea)
+        gasArea = np.array(gasArea)
+        #ax.semilogx(initialPhi - phi, fluidArea, markersize=8, marker=markerList[n], label="$N = $" + Nlist[n], lw=1.2, fillstyle='none')
+        ax.plot(phi, fluidArea, markersize=8, marker=markerList[n], label="$N = $" + Nlist[n], lw=1.2, fillstyle='none')
+    ax.tick_params(axis='both', labelsize=14)
+    ax.set_xlabel("$Global$ $density,$ $\\varphi$", fontsize=18)
+    #ax.set_xlabel("$\\varphi^{low}$ - $\\varphi$", fontsize=18)
+    ax.set_ylabel("$Fluid$ $area$", fontsize=18)
+    ax.legend(fontsize=12, loc='best')
+    fig.tight_layout()
+    figureName = "/home/francesco/Pictures/soft/mips/spRemoved-" + figureName + ".png"
+    fig.savefig(figureName + ".png", transparent=True, format = "png")
+    plt.show()
+
+def plotSPClusterArea(dirName, figureName, fixed=False, which='1e-03'):
+    fig, ax = plt.subplots(figsize=(7,4), dpi = 120)
+    if(fixed=="phi"):
+        phi = utils.readFromParams(dirName, "phi")
+        if(phi == 0.45):
+            dirList = np.array(['1e-01', '5e-02', '2e-02', '1e-02', '5e-03', '3e-03', '2e-03', '1.5e-03', '1.2e-03', '1e-03', '7e-04', '5e-04', '3e-04', '2e-04', '1.5e-04', '1e-04', '7e-05', '5e-05', '3e-05', '2e-05', '1.5e-05', '1e-05', '5e-06', '2e-06', '1.5e-06', '1e-06', '5e-07', '2e-07', '1.5e-07', '1e-07'])
+        else:
+            dirList = np.array(['1e-01', '5e-02', '2e-02', '1e-02', '5e-03', '2e-03', '1e-03', '7e-04', '5e-04', '2e-04', '1e-04', '7e-05', '5e-05', '2e-05', '1e-05', '5e-06', '2e-06', '1e-06', '5e-07', '2e-07', '1e-07'])
+        taup = np.zeros(dirList.shape[0])
+    elif(fixed=="Dr"):
+        dirList = np.array(['0.25', '0.26', '0.27', '0.28', '0.29', '0.30', '0.31', '0.32', '0.35', '0.40', '0.45', '0.50', '0.55', '0.60', '0.65', '0.70', '0.75', '0.80', '0.82', '0.84', '0.86', '0.88', '0.90', '0.92', '0.94', '0.96'])
+        colorList = cm.get_cmap('viridis', dirList.shape[0])
+        phi = np.zeros(dirList.shape[0])
+    else:
+        print('please specify fixed parameter')
+    fluidArea = np.zeros((dirList.shape[0],2))
+    gasArea = np.zeros((dirList.shape[0],2))
+    for d in range(dirList.shape[0]):
+        if(fixed=="phi"):
+            dirSample = dirName + os.sep + "iod10/active-langevin/Dr" + dirList[d] + "/dynamics/"
+            taup[d] = 1/(utils.readFromDynParams(dirSample, 'Dr')*utils.readFromDynParams(dirSample, 'sigma'))
+        elif(fixed=="Dr"):
+            dirSample = dirName + os.sep + dirList[d] + "/active-langevin/Dr" + which + "/dynamics/"
+            if(os.path.exists(dirSample + "delaunayDensity.dat")):
+                data = np.loadtxt(dirSample + "delaunayDensity.dat")
+                phi[d] = np.mean(data[:,3])
+        if(os.path.exists(dirSample + "delaunayArea.dat")):
+            data = np.loadtxt(dirSample + "delaunayArea.dat")
+            fluidArea[d,0] = np.mean(data[:,1])
+            fluidArea[d,1] = np.std(data[:,1])
+            gasArea[d,0] = np.mean(data[:,2])
+            gasArea[d,1] = np.std(data[:,2])
+            print(dirList[d], fluidArea[d,0], gasArea[d,0])
+    if(fixed=="Dr"):
+        x = phi
+        xlabel = "$Density,$ $\\varphi$"
+        figureName = "/home/francesco/Pictures/soft/mips/pClusterArea-vsPhi-" + figureName
+    elif(fixed=="phi"):
+        x = taup
+        xlabel = "$Persistence$ $time,$ $\\tau_p$"
+        figureName = "/home/francesco/Pictures/soft/mips/pClusterArea-vsDr-" + figureName
+        ax.set_xscale('log')
+    ax.tick_params(axis='both', labelsize=14)
+    ax.set_xlabel(xlabel, fontsize=18)
+    ax.set_ylabel("$Area$ $fraction$", fontsize=18)
+    ax.errorbar(x[fluidArea[:,0]>0], fluidArea[fluidArea[:,0]>0,0], fluidArea[fluidArea[:,0]>0,1], color='b', lw=1.2, marker='s', markersize = 8, fillstyle='none', elinewidth=1, capsize=4, label='$Fluid$')
+    ax.errorbar(x[gasArea[:,0]>0], gasArea[gasArea[:,0]>0,0], gasArea[gasArea[:,0]>0,1], color='g', lw=1.2, marker='o', markersize = 8, fillstyle='none', elinewidth=1, capsize=4, label='$Gas$')
+    ax.legend(fontsize=14, loc='best')
+    fig.tight_layout()
+    fig.savefig(figureName + ".png", transparent=True, format = "png")
+    plt.show()
+
+def plotSPClusterAreaVSTime(dirName, figureName, which=False):
+    fig, ax = plt.subplots(figsize=(7.5,4), dpi = 120)
+    data = np.loadtxt(dirName + "delaunayArea.dat")
+    dt = utils.readFromParams(dirName, 'dt')
+    if(which == 'fluid'):
+        figureName = figureName + '-' + which
+        ax.plot(data[:,0]*dt, data[:,1], color='b', lw=1.2, marker='s', markersize = 8, fillstyle='none', label='$Fluid$')
+    elif(which == 'gas'):
+        figureName = figureName + '-' + which
+        ax.plot(data[:,0]*dt, data[:,2], color='g', lw=1.2, marker='o', markersize = 8, fillstyle='none', label='$Gas$')
+    else:
+        ax.plot(data[:,0]*dt, data[:,1], color='b', lw=1.2, marker='s', markersize = 8, fillstyle='none', label='$Fluid$')
+        ax.plot(data[:,0]*dt, data[:,2], color='g', lw=1.2, marker='o', markersize = 8, fillstyle='none', label='$Gas$')
+    ax.tick_params(axis='both', labelsize=14)
+    ax.set_xlabel("$Simulation$ $time,$ $t$", fontsize=18)
+    ax.set_ylabel("$Area$ $fraction$", fontsize=18)
+    ax.legend(fontsize=14, loc='best')
+    figureName = "/home/francesco/Pictures/soft/mips/pArea-vsTime-" + figureName
+    fig.tight_layout()
+    fig.savefig(figureName + ".png", transparent=True, format = "png")
+    plt.show()
+
+def plotSPClusterDensity(dirName, figureName, fixed=False, which='1e-03', inter='inter'):
     fig, ax = plt.subplots(figsize=(7,4), dpi = 120)
     if(fixed=="phi"):
         phi = utils.readFromParams(dirName, "phi")
@@ -1206,59 +1325,62 @@ def plotSPClusterDensity(dirName, figureName, fixed=False, which='1e-03'):
             fluidDensity[d,1] = np.std(data[:,1])
             gasDensity[d,0] = np.mean(data[:,2])
             gasDensity[d,1] = np.std(data[:,2])
-            phi[d] = np.mean(data[:,3])
+            if(fixed=="Dr"):
+                phi[d] = np.mean(data[:,3])
             print(dirList[d], fluidDensity[d,0], gasDensity[d,0])
     if(fixed=="Dr"):
         x = phi
         xlabel = "$Density,$ $\\varphi$"
-        figureName = "/home/francesco/Pictures/soft/mips/pClusterVoro-vsPhi-" + figureName
+        figureName = "/home/francesco/Pictures/soft/mips/pClusterPhi-vsPhi-" + figureName
         ax.plot(x, x, color='k', lw=1.2, ls='--')
-        # interpolate to find upper bound
-        fluid = fluidDensity[fluidDensity[:,0]>0,0][10:]
-        error = fluidDensity[fluidDensity[:,0]>0,1][10:]
-        phiup = phi[fluidDensity[:,0]>0][10:]
-        index = np.argwhere((fluid - error*0.5) <= phiup)[0,0]
-        error = error[index]*0.5
-        p = np.linspace(phiup[index-1], phiup[index],100)
-        fluidSlope = (fluid[index] - fluid[index-1]) / (phiup[index] - phiup[index-1])
-        phiSlope = (phiup[index] - phiup[index-1]) / (phiup[index] - phiup[index-1])
-        fluidInter = fluid[index-1] + fluidSlope*(p - phiup[index-1])
-        phiInter = phiup[index-1] + phiSlope*(p - phiup[index-1])
-        phiupper = p[np.argwhere((fluidInter - error) <= phiInter)[0,0]]
-        ax.plot(phiInter, phiInter, color='r')
-        ax.plot(phiInter, fluidInter, color='r', ls='--', lw=4)
-        # interpolate to find lower bound
-        gas = gasDensity[gasDensity[:,0]>0,0][:10]
-        philow = phi[fluidDensity[:,0]>0][:10]
-        index = np.argwhere((gas + 1e-02) < philow)[0,-1]
-        p = np.linspace(philow[index-1], philow[index],100)
-        gasSlope = (gas[index] - gas[index-1]) / (philow[index] - philow[index-1])
-        phiSlope = (philow[index] - philow[index-1]) / (philow[index] - philow[index-1])
-        gasInter = gas[index-1] + gasSlope*(p - philow[index-1])
-        phiInter = philow[index-1] + phiSlope*(p - philow[index-1])
-        philower = p[np.argwhere((gasInter + 1e-02) < phiInter)[0,-1]]
-        ax.plot(phiInter, phiInter, color='r')
-        ax.plot(phiInter, gasInter, color='r', ls='--', lw=4)
-        print("phiup:", phiupper, "phidown:", philower)
-        np.savetxt(dirName + "MIPSBounds.dat", np.column_stack((numParticles, philower, phiupper)))
+        if(inter=='inter'):
+            # interpolate to find upper bound
+            fluid = fluidDensity[fluidDensity[:,0]>0,0][10:]
+            error = fluidDensity[fluidDensity[:,0]>0,1][10:]
+            phiup = phi[fluidDensity[:,0]>0][10:]
+            index = np.argwhere((fluid - error*0.5) <= phiup)[0,0]
+            error = error[index]*0.5
+            p = np.linspace(phiup[index-1], phiup[index],100)
+            fluidSlope = (fluid[index] - fluid[index-1]) / (phiup[index] - phiup[index-1])
+            phiSlope = (phiup[index] - phiup[index-1]) / (phiup[index] - phiup[index-1])
+            fluidInter = fluid[index-1] + fluidSlope*(p - phiup[index-1])
+            phiInter = phiup[index-1] + phiSlope*(p - phiup[index-1])
+            phiupper = p[np.argwhere((fluidInter - error) <= phiInter)[0,0]]
+            ax.plot(phiInter, phiInter, color='r')
+            ax.plot(phiInter, fluidInter, color='r', ls='--', lw=4)
+            # interpolate to find lower bound
+            gas = gasDensity[gasDensity[:,0]>0,0][:10]
+            philow = phi[fluidDensity[:,0]>0][:10]
+            index = np.argwhere((gas + 1e-02) < philow)[0,-1]
+            p = np.linspace(philow[index-1], philow[index],100)
+            gasSlope = (gas[index] - gas[index-1]) / (philow[index] - philow[index-1])
+            phiSlope = (philow[index] - philow[index-1]) / (philow[index] - philow[index-1])
+            gasInter = gas[index-1] + gasSlope*(p - philow[index-1])
+            phiInter = philow[index-1] + phiSlope*(p - philow[index-1])
+            philower = p[np.argwhere((gasInter + 1e-02) < phiInter)[0,-1]]
+            ax.plot(phiInter, phiInter, color='r')
+            ax.plot(phiInter, gasInter, color='r', ls='--', lw=4)
+            print("phiup:", phiupper, "phidown:", philower)
+            np.savetxt(dirName + "MIPSBounds.dat", np.column_stack((numParticles, philower, phiupper)))
+            # another way to compute the lower bound that matches the first
+            ax.set_ylim(-0.18, 1.02)
+            y = np.linspace(-0.2, 1.05, 100)
+            dirSample = dirName + os.sep + dirList[index-1] + "/active-langevin/Dr" + which + "/dynamics/"
+            phi1 = np.mean(np.loadtxt(dirSample + "delaunayDensity.dat")[:,3])
+            dirSample = dirName + os.sep + dirList[index] + "/active-langevin/Dr" + which + "/dynamics/"
+            phi2 = np.mean(np.loadtxt(dirSample + "delaunayDensity.dat")[:,3])
+            ax.plot(np.ones(100)*(phi1+phi2)/2, y, ls='dotted', color='k', lw=1)
+            print("phidown from density avarage:", (phi1+phi2)/2)
     elif(fixed=="phi"):
         x = taup
         xlabel = "$Persistence$ $time,$ $\\tau_p$"
-        figureName = "/home/francesco/Pictures/soft/mips/pClusterVoro-vsDr-" + figureName
+        figureName = "/home/francesco/Pictures/soft/mips/pClusterPhi-vsDr-" + figureName
     ax.tick_params(axis='both', labelsize=14)
     ax.set_xlabel(xlabel, fontsize=18)
     ax.set_ylabel("$Area$ $fraction$", fontsize=18)
     ax.errorbar(x[fluidDensity[:,0]>0], fluidDensity[fluidDensity[:,0]>0,0], fluidDensity[fluidDensity[:,0]>0,1], color='b', lw=1.2, marker='s', markersize = 8, fillstyle='none', elinewidth=1, capsize=4, label='$Fluid$')
     ax.errorbar(x[gasDensity[:,0]>0], gasDensity[gasDensity[:,0]>0,0], gasDensity[gasDensity[:,0]>0,1], color='g', lw=1.2, marker='o', markersize = 8, fillstyle='none', elinewidth=1, capsize=4, label='$Gas$')
     ax.legend(fontsize=14, loc='best')
-    ax.set_ylim(-0.18, 1.02)
-    y = np.linspace(-0.2, 1.05, 100)
-    dirSample = dirName + os.sep + "0.28/active-langevin/Dr" + which + "/dynamics/"
-    phi1 = np.mean(np.loadtxt(dirSample + "delaunayDensity.dat")[:,3])
-    dirSample = dirName + os.sep + "0.29/active-langevin/Dr" + which + "/dynamics/"
-    phi2 = np.mean(np.loadtxt(dirSample + "delaunayDensity.dat")[:,3])
-    ax.plot(np.ones(100)*(phi1+phi2)/2, y, ls='dotted', color='k', lw=1)
-    print("phidown from density avarage:", (phi1+phi2)/2)
     if(fixed!="Dr"):
         ax.set_xscale('log')
     #ax.set_xlim(5.8e-06, 2.8e03)
@@ -1267,7 +1389,7 @@ def plotSPClusterDensity(dirName, figureName, fixed=False, which='1e-03'):
     plt.show()
 
 def plotSPClusterDensityVSTime(dirName, figureName, which=False):
-    fig, ax = plt.subplots(figsize=(7,7), dpi = 120)
+    fig, ax = plt.subplots(figsize=(7.5,4), dpi = 120)
     data = np.loadtxt(dirName + "delaunayDensity.dat")
     dt = utils.readFromParams(dirName, 'dt')
     if(which == 'fluid'):
@@ -1281,9 +1403,9 @@ def plotSPClusterDensityVSTime(dirName, figureName, which=False):
         ax.plot(data[:,0]*dt, data[:,2], color='g', lw=1.2, marker='o', markersize = 8, fillstyle='none', label='$Gas$')
     ax.tick_params(axis='both', labelsize=14)
     ax.set_xlabel("$Simulation$ $time,$ $t$", fontsize=18)
-    ax.set_ylabel("$Area$ $fraction$", fontsize=18)
+    ax.set_ylabel("$Density$", fontsize=18)
     ax.legend(fontsize=14, loc='best')
-    figureName = "/home/francesco/Pictures/soft/mips/pFraction-vsTime-" + figureName
+    figureName = "/home/francesco/Pictures/soft/mips/pDensity-vsTime-" + figureName
     fig.tight_layout()
     fig.savefig(figureName + ".png", transparent=True, format = "png")
     plt.show()
@@ -1915,8 +2037,8 @@ def plotSPDeltaPVSSystemSize(dirName, figureName, which='1.5e-04'):
 def plotSPMIPSBoundsVSSystemSize(dirName, figureName, which='up'):
     fig, ax = plt.subplots(figsize = (7,3.5), dpi = 120)
     labelList = np.array(['$N = 1024$', '$N = 2048$', '$N = 4096$', '$N = 8192$', '$N = 16384$', '$N = 32768$'])
-    dirList = np.array(['1024', '2048', '4096', '8192', '16384'])#, '32768'])
-    numParticles = np.array([1024, 2048, 4096, 8192, 16384])#, 32768])
+    dirList = np.array(['1024', '2048', '4096', '8192', '16384', '32768'])
+    numParticles = np.array([1024, 2048, 4096, 8192, 16384, 32768])
     bounds = np.zeros((dirList.shape[0],2))
     for d in range(dirList.shape[0]):
         dirSample = dirName + os.sep + dirList[d] + "-2d/densitySweep/"
@@ -2224,9 +2346,10 @@ if __name__ == '__main__':
 ############################ local density analysis ############################
     if(whichPlot == "simplex"):
         figureName = sys.argv[3]
-        pad = float(sys.argv[4])
-        logy = sys.argv[5]
-        plotSimplexDensity(dirName, figureName, pad, logy)
+        which = sys.argv[4]
+        pad = float(sys.argv[5])
+        logy = sys.argv[6]
+        plotSimplexDensity(dirName, figureName, which, pad, logy)
 
     elif(whichPlot == "fitphi"):
         figureName = sys.argv[3]
@@ -2354,11 +2477,27 @@ if __name__ == '__main__':
         which = sys.argv[5]
         plotSPPressureProfile(dirName, figureName, shift, which)
 
+    elif(whichPlot == "remove"):
+        figureName = sys.argv[3]
+        plotSPAreaVSRemoved(dirName, figureName)
+
+    elif(whichPlot == "clusterarea"):
+        figureName = sys.argv[3]
+        fixed = sys.argv[4]
+        which = sys.argv[5]
+        plotSPClusterArea(dirName, figureName, fixed, which)
+
+    elif(whichPlot == "areatime"):
+        figureName = sys.argv[3]
+        which = sys.argv[4]
+        plotSPClusterAreaVSTime(dirName, figureName, which)
+
     elif(whichPlot == "clusterphi"):
         figureName = sys.argv[3]
         fixed = sys.argv[4]
         which = sys.argv[5]
-        plotSPClusterDensity(dirName, figureName, fixed, which)
+        inter = sys.argv[6]
+        plotSPClusterDensity(dirName, figureName, fixed, which, inter)
 
     elif(whichPlot == "phitime"):
         figureName = sys.argv[3]

spCluster.py
@@ -1448,6 +1448,29 @@ def checkDelaunay(dirName):
     delaunay = Delaunay(pos)
     checkDelaunayInclusivity(delaunay.simplices, pos, rad, boxSize)
 
+################### Cluster augmented packing for plotting #####################
+def computeAugmentedDelaunayCluster(dirName, threshold1=0.78, threshold2=0.45, shiftx=0, shifty=0):
+    sep = utils.getDirSep(dirName, "boxSize")
+    numParticles = int(utils.readFromParams(dirName + sep, "numParticles"))
+    boxSize = np.array(np.loadtxt(dirName + sep + "boxSize.dat"))
+    rad = np.array(np.loadtxt(dirName + sep + "particleRad.dat"))
+    pos = utils.getPBCPositions(dirName + os.sep + "particlePos.dat", boxSize)
+    pos = utils.shiftPositions(pos, boxSize, shiftx, shifty)
+    newPos, newRad, newIndices = utils.augmentPacking(pos, rad)
+    delaunay = Delaunay(newPos)
+    simplices = delaunay.simplices
+    simplexDensity, _ = utils.computeDelaunayDensity(simplices, newPos, newRad, boxSize*1.5)
+    print(np.argwhere(simplexDensity<0)[:,0])
+    insideIndex = utils.getInsideBoxDelaunaySimplices(simplices, newPos, boxSize)
+    simplexLabelList = np.ones(simplexDensity.shape[0])
+    for simplexId in range(simplexLabelList.shape[0]):
+        if(simplexDensity[simplexId] > threshold1):
+            simplexLabelList[simplexId] = 0
+        if(simplexDensity[simplexId] < threshold1 and simplexDensity[simplexId] > threshold2):
+            simplexLabelList[simplexId] = 0.5
+    insideIndex = utils.getInsideBoxDelaunaySimplices(simplices, newPos, boxSize)
+    return newPos, simplices[insideIndex==1], simplexLabelList[insideIndex==1], simplexDensity[insideIndex==1]
+
 ############################## Delaunay clustering #############################
 def computeDelaunayCluster(dirName, threshold=0.78, filter=False, plot=False):
     sep = utils.getDirSep(dirName, "boxSize")
@@ -1464,12 +1487,16 @@ def computeDelaunayCluster(dirName, threshold=0.78, filter=False, plot=False):
     simplices = utils.getPBCDelaunay(pos, rad, boxSize)
     # compute delaunay densities
     simplexDensity, simplexArea = utils.computeDelaunayDensity(simplices, pos, rad, boxSize)
+    if(np.argwhere(simplexDensity<0)[:,0].shape[0] > 0):
+        print("There are", np.argwhere(simplexDensity<0)[:,0].shape[0], "negative simplex densities")
+        print(simplices[np.argwhere(simplexDensity<0)[:,0]])
     denseSimplexList = np.zeros(simplexDensity.shape[0])
     #print("average local density:", np.mean(simplexDensity))
     # first find dense simplices
     for i in range(simplexDensity.shape[0]):
         if(simplexDensity[i] > threshold):
             denseSimplexList[i] = 1
+    #print("Fraction of dense simplices: ", denseSimplexList[denseSimplexList==1].shape[0]/denseSimplexList.shape[0])
     # if all the simplices touching a particle are dense then the particle is dense
     for i in range(numParticles):
         indices = np.argwhere(simplices==i)[:,0]
@@ -1479,9 +1506,9 @@ def computeDelaunayCluster(dirName, threshold=0.78, filter=False, plot=False):
             if(denseSimplexList[sIndex] == 1):
                 denseList[i] = 1
         #        count += 1
-        #if(count > maxCount-4):
+        #if(count > int(maxCount / 1.5)):
         #    denseList[i] = 1
-    #print("Number of dense particles: ", denseList[denseList==1].shape[0])
+    #print("Fraction of dense particles: ", denseList[denseList==1].shape[0]/denseList.shape[0])
     if(filter=='filter'):
         connectList = np.zeros(numParticles)
         for i in range(numParticles):
@@ -1614,7 +1641,7 @@ def computeClusterDelaunayDensity(dirName, plot=False, dirSpacing=1):
             simplexDensity = np.loadtxt(dirSample + os.sep + "simplexDensity.dat")
             simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
         else:
-            _, simplexDensity = computeDelaunayCluster(dirSample)
+            _, simplexDensity = computeDelaunayCluster(dirSample, threshold=0.78, filter='filter')
             denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
             simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
         occupiedArea = simplexDensity * simplexArea
@@ -1649,6 +1676,44 @@ def computeClusterDelaunayDensity(dirName, plot=False, dirSpacing=1):
         uplot.plotCorrelation(timeList, delaunayDensity[:,2], "$\\varphi^{Delaunay}$", xlabel = "$Time,$ $t$", color='k')
         plt.pause(0.5)
         #plt.show()
+
+######################## Compute cluster shape parameter #######################
+def computeClusterDelaunayArea(dirName, plot=False, dirSpacing=1):
+    numParticles = int(utils.readFromParams(dirName, "numParticles"))
+    sep = utils.getDirSep(dirName, "boxSize")
+    boxSize = np.array(np.loadtxt(dirName + sep + "boxSize.dat"))
+    rad = np.array(np.loadtxt(dirName + sep + "particleRad.dat"))
+    dirList, timeList = utils.getOrderedDirectories(dirName)
+    timeList = timeList.astype(int)
+    dirList = dirList[np.argwhere(timeList%dirSpacing==0)[:,0]]
+    timeList = timeList[np.argwhere(timeList%dirSpacing==0)[:,0]]
+    area = np.zeros((dirList.shape[0],2))
+    for d in range(dirList.shape[0]):
+        dirSample = dirName + os.sep + dirList[d]
+        pos = utils.getPBCPositions(dirSample + "/particlePos.dat", boxSize)
+        contacts = np.loadtxt(dirSample + "/particleContacts.dat").astype(np.int64)
+        # area
+        if(os.path.exists(dirSample + os.sep + "denseSimplexList.dat")):
+            denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
+            simplexDensity = np.loadtxt(dirSample + os.sep + "simplexDensity.dat")
+            simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
+        else:
+            _, simplexDensity = computeDelaunayCluster(dirSample)
+            denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
+            simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
+        occupiedArea = simplexDensity * simplexArea
+        for i in range(simplexDensity.shape[0]):
+            if(denseSimplexList[i]==1):
+                area[d,0] += simplexArea[i]
+            else:
+                area[d,1] += simplexArea[i]
+    np.savetxt(dirName + os.sep + "delaunayArea.dat", np.column_stack((timeList, area)))
+    print("Fluid area: ", np.mean(area[:,0]), " +- ", np.std(area[:,0]))
+    print("Gas area: ", np.mean(area[:,1]), " +- ", np.std(area[:,1]))
+    if(plot=='plot'):
+        uplot.plotCorrelation(timeList, area[:,0], "$Area$ $fraction$", xlabel = "$Time,$ $t$", color='b')
+        uplot.plotCorrelation(timeList, area[:,1], "$Area$ $fraction$", xlabel = "$Time,$ $t$", color='g')
+        plt.pause(0.5)
 
 ######################## Compute delaunay cluster border #######################
 def computeDelaunayBorder(dirName, threshold=0.85, filter='filter'):
@@ -1764,7 +1829,7 @@ def computeDelaunayClusterPressureVSTime(dirName, dirSpacing=1):
             denseList, _ = computeDelaunayCluster(dirSample)
             denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
             simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
-        # first fill out the area occupied by fluid, gas and interface
+        # first fill out the area occupied by fluid and the gas
         fluidArea = 0
         gasArea = 0
         for i in range(simplexArea.shape[0]):
@@ -3071,6 +3136,10 @@ if __name__ == '__main__':
     elif(whichCorr == "deldensity"):
         plot = sys.argv[3]
         computeClusterDelaunayDensity(dirName, plot)
+
+    elif(whichCorr == "delarea"):
+        plot = sys.argv[3]
+        computeClusterDelaunayArea(dirName, plot)
 
     elif(whichCorr == "delshape"):
         plot = sys.argv[3]

utils.py
@@ -8,6 +8,7 @@ from scipy.fft import fft, fftfreq, fft2
 from sklearn.cluster import DBSCAN
 from scipy.spatial import Delaunay
 import spCluster as cluster
+import random
 import os
 
 ############################## general utilities ###############################
@@ -726,6 +727,18 @@ def initializeDroplet(dirName, dirSave):
     pos[:,1] -= np.floor(pos[:,1]/boxSize[1]) * boxSize[1]
     np.savetxt(dirSave + '/particlePos.dat', pos)
 
+def getUniqueRandomList(low, high, num):
+    numRandom = 0
+    randomList = [0]
+    while(numRandom < num-1):
+        rand = np.random.randint(low, high)
+        if rand not in randomList:
+            randomList.append(rand)
+            numRandom += 1
+    randomList = np.array(randomList)
+    print(randomList.shape[0], np.unique(randomList).shape[0])
+    return randomList
+
 def removeParticles(dirName, numRemove):
     # load all the packing files
     boxSize = np.loadtxt(dirName + '/boxSize.dat')
@@ -744,8 +757,8 @@ def removeParticles(dirName, numRemove):
     maxRemove = gasIndices.shape[0]
     print("Maximum number of removable particles:", maxRemove)
     if(numRemove < maxRemove):
-        random = np.random.randint(0, maxRemove, numRemove)
-        removeIndices = gasIndices[random]
+        randomList = getUniqueRandomList(0, maxRemove, numRemove)
+        removeIndices = gasIndices[randomList]
         rad = np.delete(rad, removeIndices)
         pos = np.delete(pos, removeIndices, axis=0)
         vel = np.delete(vel, removeIndices)
@@ -765,7 +778,7 @@ def removeParticles(dirName, numRemove):
         print("Please remove a number of particles smaller than", maxRemove)
 
 ############################### Delaunay analysis ##############################
-def augmentPacking(pos, rad, fraction=0.05):
+def augmentPacking(pos, rad, fraction=0.5):
     # augment packing by copying a fraction of the particles around the walls
     Lx = np.array([1,0])
     Ly = np.array([0,1])
@@ -889,10 +902,19 @@ def computeIntersectionArea(pos0, pos1, pos2, sigma, boxSize):
     #projLength = np.sqrt((slope**2 * pos2[0]**2 + pos2[1]**2 + intercept**2 - 2*intercept*pos2[1] - 2*slope*pos2[0]*pos2[1] + 2*slope*intercept*pos2[1]) / (1 + slope**2))
     theta = np.arcsin(projLength / np.linalg.norm(pos2))
     intersectArea = 0.5 * sigma**2 * theta
-    return projLength, intersectArea
+    # check if distances from two opposite vertices to the projection point are both less than the distance between the two opposite vertices
+    delta10 = np.linalg.norm(pos0)
+    delta12 = np.linalg.norm(pos2)
+    delta20 = np.linalg.norm(pbcDistance(pos2, pos0, boxSize))
+    delta1Proj = delta12 * np.cos(np.arcsin(projLength / delta12)) # this angle is the same as theta
+    delta0Proj = delta20 * np.cos(np.arcsin(projLength / delta20))
+    internalProj = True
+    if(delta1Proj > delta10 or delta0Proj > delta10):
+        internalProj = False
+    return projLength, intersectArea, internalProj
 
-def checkSegmentArea(projLength, sigma):
-    if(projLength < sigma):
+def checkSegmentArea(projLength, sigma, internalProj):
+    if(projLength < sigma and internalProj == True):
         smallTheta = np.arccos(projLength/sigma)
         smallTheta /= 2
         return smallTheta * sigma**2 - 0.5 * sigma**2 * np.sin(2*smallTheta)
@@ -915,10 +937,6 @@ def isSimplexNearWall(pIndexList, pos, rad, boxSize):
     return isSimplexNearWall
 
 def computeTriangleArea(pos0, pos1, pos2, boxSize):
-    #pos2 = pbcDistance(pos2, pos1, boxSize)
-    #pos0 = pbcDistance(pos0, pos1, boxSize)
-    #pos1 = np.zeros(pos1.shape[0])
-    #return 0.5 * np.abs(pos0[0]*(pos1[1] - pos2[1]) + pos1[0]*(pos2[1] - pos0[1]) + pos2[0]*(pos0[1] - pos1[1]))
     delta01 = np.linalg.norm(pbcDistance(pos0, pos1, boxSize))
     delta12 = np.linalg.norm(pbcDistance(pos1, pos2, boxSize))
     delta20 = np.linalg.norm(pbcDistance(pos2, pos0, boxSize))
@@ -939,14 +957,14 @@ def computeDelaunayDensity(simplices, pos, rad, boxSize):
         # first compute projection distance for each vertex in the simplex and then check if the intersection is all inside the simplex
         # if not, remove the external segment from the intersection area and add it to the simplex where the segment is contained
         # first vertex
-        projLength, intersectArea1 = computeIntersectionArea(pos0, pos1, pos2, rad[simplices[sIndex,1]], boxSize)
-        segmentArea2 = checkSegmentArea(projLength, rad[simplices[sIndex,2]])
+        projLength, intersectArea1, internalProj = computeIntersectionArea(pos0, pos1, pos2, rad[simplices[sIndex,1]], boxSize)
+        segmentArea2 = checkSegmentArea(projLength, rad[simplices[sIndex,2]], internalProj)
         # second vertex
-        projLength, intersectArea2 = computeIntersectionArea(pos1, pos2, pos0, rad[simplices[sIndex,2]], boxSize)
-        segmentArea0 = checkSegmentArea(projLength, rad[simplices[sIndex,0]])
+        projLength, intersectArea2, internalProj = computeIntersectionArea(pos1, pos2, pos0, rad[simplices[sIndex,2]], boxSize)
+        segmentArea0 = checkSegmentArea(projLength, rad[simplices[sIndex,0]], internalProj)
         # third vertex
-        projLength, intersectArea0 = computeIntersectionArea(pos2, pos0, pos1, rad[simplices[sIndex,0]], boxSize)
-        segmentArea1 = checkSegmentArea(projLength, rad[simplices[sIndex,1]])
+        projLength, intersectArea0, internalProj = computeIntersectionArea(pos2, pos0, pos1, rad[simplices[sIndex,0]], boxSize)
+        segmentArea1 = checkSegmentArea(projLength, rad[simplices[sIndex,1]], internalProj)
         # first correction
         if(segmentArea2 > 0):
             oppositeIndex = findOppositeSimplexIndex(simplices, sIndex, simplices[sIndex,0], simplices[sIndex,1])

visuals.py
@@ -141,7 +141,7 @@ def plotSPPacking(dirName, figureName, ekmap=False, quiver=False, dense=False, b
     yBounds = np.array([0, boxSize[1]])
     #denseList = np.loadtxt(dirName + os.sep + "denseList.dat")
     pos = utils.centerPositions(pos, rad, boxSize)
-    pos = utils.shiftPositions(pos, boxSize, 0, 0.1)
+    #pos = utils.shiftPositions(pos, boxSize, 0.1, -0.3)
     fig = plt.figure(0, dpi = 150)
     ax = fig.gca()
     ax.set_xlim(xBounds[0], xBounds[1])
@@ -226,7 +226,7 @@ def plotSPPacking(dirName, figureName, ekmap=False, quiver=False, dense=False, b
     else:
         figureName = "/home/francesco/Pictures/soft/packings/" + figureName + ".png"
     plt.tight_layout()
-    plt.savefig(figureName, transparent=True, format = "png")
+    plt.savefig(figureName, transparent=False, format = "png")
     plt.show()
 
 def plotSPFixedBoundaryPacking(dirName, figureName, onedim=False, quiver=False, alpha = 0.6):
@@ -364,10 +364,10 @@ def plotSPVoronoiPacking(dirName, figureName, dense=False, threshold=0.84, filte
     setPackingAxes(boxSize, ax)
     colorId = getRadColorList(rad)
     if(dense==True):
-        if(os.path.exists(dirName + os.sep + "delaunayList!.dat")):
-            denseList = np.loadtxt(dirName + os.sep + "delaunayList.dat")
+        if(os.path.exists(dirName + os.sep + "denseList!.dat")):
+            denseList = np.loadtxt(dirName + os.sep + "denseList.dat")
         else:
-            denseList,_ = cluster.computeDelaunayCluster(dirName, threshold, filter=filter)
+            denseList,_ = cluster.computeVoronoiCluster(dirName, threshold, filter=filter)
         colorId = getDenseColorList(denseList)
     for particleId in range(rad.shape[0]):
         x = pos[particleId,0]
@@ -383,14 +383,30 @@ def plotSPVoronoiPacking(dirName, figureName, dense=False, threshold=0.84, filte
     plt.savefig(figureName, transparent=False, format = "png")
     plt.show()
 
-def plotSPDelaunayPacking(dirName, figureName, dense=False, threshold=0.84, filter=True, alpha=0.8):
+def getDenseSimplexColorList(denseList):
+    colorId = np.ones(denseList.shape[0])
+    for simplexId in range(denseList.shape[0]):
+        if(denseList[simplexId]==0):
+            colorId[simplexId] = 0
+    return colorId
+
+def computeDenseSimplexColorList(densityList):
+    colorId = np.ones(densityList.shape[0])
+    for simplexId in range(densityList.shape[0]):
+        if(densityList[simplexId] > 0.78):
+            colorId[simplexId] = 0
+        if(densityList[simplexId] < 0.78 and densityList[simplexId] > 0.453):
+            colorId[simplexId] = 0.5
+    return colorId
+
+def plotSPDelaunayPacking(dirName, figureName, dense=False, threshold=0.78, filter=False, alpha=0.8):
     sep = utils.getDirSep(dirName, "boxSize")
     boxSize = np.loadtxt(dirName + sep + "boxSize.dat")
     xBounds = np.array([0, boxSize[0]])
     yBounds = np.array([0, boxSize[1]])
     rad = np.array(np.loadtxt(dirName + sep + "particleRad.dat"))
     pos = utils.getPBCPositions(dirName + os.sep + "particlePos.dat", boxSize)
-    pos = utils.shiftPositions(pos, boxSize, 0, -0.4)
+    pos = utils.shiftPositions(pos, boxSize, 0, 0)
     fig = plt.figure(0, dpi = 150)
     ax = fig.gca()
     ax.set_xlim(xBounds[0], xBounds[1])
@@ -398,22 +414,33 @@ def plotSPDelaunayPacking(dirName, figureName, dense=False, threshold=0.84, filt
     ax.set_aspect('equal', adjustable='box')
     setPackingAxes(boxSize, ax)
     #setBigBoxAxes(boxSize, ax, 0.1)
-    newPos, newRad, newIndices = utils.augmentPacking(pos, rad)
-    colorId = getRadColorList(newRad)
+    colorId = getRadColorList(rad)
     if(dense==True):
-        if(os.path.exists(dirName + os.sep + "delaunayList!.dat")):
+        if(os.path.exists(dirName + os.sep + "delaunayList.dat")):
             denseList = np.loadtxt(dirName + os.sep + "delaunayList.dat")
         else:
             denseList,_ = cluster.computeDelaunayCluster(dirName, threshold, filter=filter)
+        denseSimplexList = np.loadtxt(dirName + os.sep + "denseSimplexList.dat")
         colorId = getDenseColorList(denseList)
     for particleId in range(rad.shape[0]):
         x = pos[particleId,0]
         y = pos[particleId,1]
         r = rad[particleId]
-        ax.add_artist(plt.Circle([x, y], r, edgecolor='k', facecolor=colorId[particleId], alpha=alpha, linewidth=0.3))
-    delaunay = Delaunay(newPos)
-    insideIndex = utils.getInsideBoxDelaunaySimplices(delaunay.simplices, newPos, boxSize)
-    plt.triplot(newPos[:,0], newPos[:,1], delaunay.simplices[insideIndex==1], lw=0.3, color='k')
+        ax.add_artist(plt.Circle([x, y], r, edgecolor='k', facecolor=colorId[particleId], alpha=0.5, linewidth=0.3))
+    #delaunay = Delaunay(newPos)
+    #insideIndex = utils.getInsideBoxDelaunaySimplices(delaunay.simplices, newPos, boxSize)
+    #plt.triplot(newPos[:,0], newPos[:,1], delaunay.simplices[insideIndex==1], lw=0.2, color='k')
+    #newPos, simplices, colorId, simplexDensity = cluster.computeAugmentedDelaunayCluster(dirName, threshold, 0.45, 0, 0)
+    #plt.tripcolor(newPos[:,0], newPos[:,1], simplices[simplexDensity<0], lw=0.3, facecolors=colorId[simplexDensity<0], edgecolors='k', alpha=0.5, cmap='bwr')
+    simplices = utils.getPBCDelaunay(pos, rad, boxSize)
+    simplexDensity = np.loadtxt(dirName + os.sep + "simplexDensity.dat")
+    print(np.argwhere(simplexDensity<0))
+    print(simplexDensity[simplexDensity<0])
+    print(simplices[simplexDensity<0])
+    plt.triplot(pos[:,0], pos[:,1], simplices[simplexDensity<0], lw=0.2, color='k')
+    plt.plot(pos[simplices[simplexDensity<0,0],0], pos[simplices[simplexDensity<0,0],1], marker='*', markersize=20, color='r')
+    plt.plot(pos[simplices[simplexDensity<0,1],0], pos[simplices[simplexDensity<0,1],1], marker='*', markersize=20, color='b')
+    plt.plot(pos[simplices[simplexDensity<0,2],0], pos[simplices[simplexDensity<0,2],1], marker='*', markersize=20, color='g')
     #plt.plot(pos[:,0], pos[:,1], 'o', markersize=0.2, markeredgecolor='k', color='r')
     #plt.plot(pos[14730,0], pos[14730,1], '*', markersize=10, markeredgecolor='k', color='b')
     #plt.plot(pos[595,0], pos[595,1], '*', markersize=10, markeredgecolor='k', color='r')