fixed a few bugs in surface tension calculation

spCluster.py
@@ -1658,7 +1658,7 @@ def computeClusterDelaunayDensity(dirName, plot=False, dirSpacing=1):
         dirSample = dirName + os.sep + dirList[d]
         pos = utils.getPBCPositions(dirSample + "/particlePos.dat", boxSize)
         contacts = np.loadtxt(dirSample + "/particleContacts.dat").astype(np.int64)
-        if(os.path.exists(dirSample + os.sep + "denseSimplexList!.dat")):
+        if(os.path.exists(dirSample + os.sep + "denseSimplexList.dat")):
             denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
             simplexDensity = np.loadtxt(dirSample + os.sep + "simplexDensity.dat")
             simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
@@ -1696,8 +1696,8 @@ def computeClusterDelaunayDensity(dirName, plot=False, dirSpacing=1):
         uplot.plotCorrelation(timeList, delaunayDensity[:,0], "$\\varphi^{Delaunay}$", xlabel = "$Time,$ $t$", color='b')
         uplot.plotCorrelation(timeList, delaunayDensity[:,1], "$\\varphi^{Delaunay}$", xlabel = "$Time,$ $t$", color='g')
         uplot.plotCorrelation(timeList, delaunayDensity[:,2], "$\\varphi^{Delaunay}$", xlabel = "$Time,$ $t$", color='k')
-        plt.pause(0.5)
-        #plt.show()
+        #plt.pause(0.5)
+        plt.show()
 
 ######################## Compute cluster shape parameter #######################
 def computeClusterDelaunayArea(dirName, plot=False, dirSpacing=1):
@@ -1843,12 +1843,12 @@ def computeDelaunayClusterPressureVSTime(dirName, dirSpacing=1):
     boxLength = 2 * (boxSize[0] + boxSize[1])
     for d in range(dirList.shape[0]):
         dirSample = dirName + os.sep + dirList[d]
-        if(os.path.exists(dirSample + os.sep + "delaunayList.dat")):
+        if(os.path.exists(dirSample + os.sep + "delaunayList!.dat")):
             denseList = np.loadtxt(dirSample + os.sep + "delaunayList.dat")
             denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
             simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
         else:
-            denseList, _ = computeDelaunayCluster(dirSample)
+            denseList, _ = computeDelaunayCluster(dirSample, threshold=0.78, filter='filter')
             denseSimplexList = np.loadtxt(dirSample + os.sep + "denseSimplexList.dat")
             simplexArea = np.loadtxt(dirSample + os.sep + "simplexArea.dat")
         # first fill out the area occupied by fluid and the gas
@@ -2394,9 +2394,9 @@ def averageRadialTension(dirName, shiftx, shifty, dirSpacing=1):
         virial[:,i] *= sigma/dirList.shape[0]
         active[:,i] *= sigma/dirList.shape[0]
     np.savetxt(dirName + os.sep + "surfaceTension.dat", np.column_stack((centers, virial, active)))
-    print("surface tension: ", np.sum(centers/sigma * (virial[:,0] + active[:,0] - virial[:,1] - active[:,1])))
-    uplot.plotCorrelation(centers, virial[:,0] - virial[:,1], "$\\Delta p$", xlabel = "$Distance$", color='k', lw=1.5)
-    uplot.plotCorrelation(centers, active[:,0] - active[:,1], "$\\Delta p$", xlabel = "$Distance$", color=[1,0.5,0], lw=1.5)
+    print("surface tension: ", np.sum(virial[:,0] + active[:,0] - virial[:,1] - active[:,1]))
+    uplot.plotCorrelation(centers, virial[:,0] - virial[:,1], "$Surface$ $tension$", xlabel = "$Distance$", color='k', lw=1.5)
+    uplot.plotCorrelation(centers, active[:,0] - active[:,1], "$Surface$ $tension$", xlabel = "$Distance$", color=[1,0.5,0], lw=1.5)
     #plt.yscale('log')
     plt.show()
 
@@ -2473,7 +2473,7 @@ def averageLinearPressureProfile(dirName, shiftx=0, dirSpacing=1):
     plt.show()
 
 ############################ Linear surface tension ############################
-def averageLinearTension(dirName, shiftx=0, dirSpacing=1):
+def averageLinearTension(dirName, dirSpacing=1):
     dim = 2
     ec = 240
     Dr = float(utils.readFromDynParams(dirName, "Dr"))
@@ -2497,7 +2497,6 @@ def averageLinearTension(dirName, shiftx=0, dirSpacing=1):
     for d in range(dirList.shape[0]):
         dirSample = dirName + os.sep + dirList[d]
         pos = utils.getPBCPositions(dirSample + "/particlePos.dat", boxSize)
-        pos = utils.shiftPositions(pos, boxSize, shiftx, 0)
         #pos = np.loadtxt(dirSample + "/particlePos.dat")
         vel = np.loadtxt(dirSample + "/particleVel.dat")
         angle = utils.getMOD2PIAngles(dirSample + "/particleAngles.dat")
@@ -2524,12 +2523,12 @@ def averageLinearTension(dirName, shiftx=0, dirSpacing=1):
                     virial[j,1] += worky
                     active[j,0] += driving * vel[i,0] * director[i,0] / (2*Dr)
                     active[j,1] += driving * vel[i,1] * director[i,1] / (2*Dr)
-    virial *= sigma**2/(binArea * dirList.shape[0])
-    active *= sigma**2/(binArea * dirList.shape[0])
+    virial *= sigma*binWidth/(binArea * dirList.shape[0])
+    active *= sigma*binWidth/(binArea * dirList.shape[0])
     np.savetxt(dirName + os.sep + "surfaceTension.dat", np.column_stack((centers, virial, active)))
-    print("surface tension: ", np.sum(centers/sigma*(virial[:,0] + active[:,0] - virial[:,1] - active[:,1])))
-    uplot.plotCorrelation(centers, virial[:,0] - virial[:,1], "$\\Delta p$", xlabel = "$Distance$", color='k', lw=1.5)
-    uplot.plotCorrelation(centers, active[:,0] - active[:,1], "$\\Delta p$", xlabel = "$Distance$", color=[1,0.5,0], lw=1.5)
+    print("surface tension: ", 0.5*np.sum(virial[:,0] + active[:,0] - virial[:,1] - active[:,1]))
+    uplot.plotCorrelation(centers, virial[:,0] - virial[:,1], "$Surface$ $tension$", xlabel = "$Distance$", color='k', lw=1.5)
+    uplot.plotCorrelation(centers, active[:,0] - active[:,1], "$Surface$ $tension$", xlabel = "$Distance$", color=[1,0.5,0], lw=1.5)
     #plt.yscale('log')
     plt.show()
 
@@ -2862,8 +2861,8 @@ def averageDropletRadialTension(dirName, shiftx, shifty, l1=0.04, dirSpacing=1):
         thermal[:,i] *= sigma/dirList.shape[0]
     np.savetxt(dirName + os.sep + "surfaceTension.dat", np.column_stack((centers, virial, thermal)))
     print("surface tension: ", np.sum(centers * (virial[:,0] + thermal[:,0] - virial[:,1] - thermal[:,1])))
-    uplot.plotCorrelation(centers, virial[:,0] - virial[:,1], "$\\Delta p$", xlabel = "$Distance$", color='k', lw=1.5)
-    uplot.plotCorrelation(centers, thermal[:,0] - thermal[:,1], "$\\Delta p$", xlabel = "$Distance$", color='r', lw=1.5)
+    uplot.plotCorrelation(centers, virial[:,0] - virial[:,1], "$Surface$ $tension$", xlabel = "$Distance$", color='k', lw=1.5)
+    uplot.plotCorrelation(centers, thermal[:,0] - thermal[:,1], "$Surface$ $tension$", xlabel = "$Distance$", color='r', lw=1.5)
     #plt.yscale('log')
     plt.show()
 
@@ -2987,9 +2986,9 @@ def averageDropletLinearTension(dirName, l1=0.04, dirSpacing=1):
     thermal *= sigma*binWidth/(binArea * dirList.shape[0])
     virial *= sigma*binWidth/(binArea * dirList.shape[0])
     np.savetxt(dirName + os.sep + "surfaceTension.dat", np.column_stack((centers, virial, thermal)))
-    print("surface tension: ", np.sum(centers * (virial[:,0] + thermal[:,0] - virial[:,1] - thermal[:,1])))
-    uplot.plotCorrelation(centers, virial[:,0] - virial[:,1], "$\\Delta p$", xlabel = "$Distance$", color='k', lw=1.5)
-    uplot.plotCorrelation(centers, thermal[:,0] - thermal[:,1], "$\\Delta p$", xlabel = "$Distance$", color='r', lw=1.5)
+    print("surface tension: ", 0.5*np.sum(virial[:,0] + thermal[:,0] - virial[:,1] - thermal[:,1]))
+    uplot.plotCorrelation(centers, virial[:,0] - virial[:,1], "$Surface$ $tension$", xlabel = "$Distance$", color='k', lw=1.5)
+    uplot.plotCorrelation(centers, thermal[:,0] - thermal[:,1], "$Surface$ $tension$", xlabel = "$Distance$", color='r', lw=1.5)
     #plt.yscale('log')
     plt.show()
 
@@ -3194,12 +3193,10 @@ if __name__ == '__main__':
         averageRadialTension(dirName, shiftx, shifty)
 
     elif(whichCorr == "linearprofile"):
-        shiftx = float(sys.argv[3])
-        averageLinearPressureProfile(dirName, shiftx)
+        averageLinearPressureProfile(dirName)
 
     elif(whichCorr == "lineartension"):
-        shiftx = float(sys.argv[3])
-        averageLinearTension(dirName, shiftx)
+        averageLinearTension(dirName)
 
     elif(whichCorr == "ptime"):
         bound = sys.argv[3]
@@ -3234,11 +3231,11 @@ if __name__ == '__main__':
         l1 = float(sys.argv[5])
         averageDropletRadialTension(dirName, shiftx, shifty, l1)
 
-    elif(whichCorr == "slabprofile"):
+    elif(whichCorr == "droplinearprofile"):
         l1 = float(sys.argv[3])
         averageDropletLinearPressureProfile(dirName, l1)
 
-    elif(whichCorr == "slabtension"):
+    elif(whichCorr == "droplineartension"):
         l1 = float(sys.argv[3])
         averageDropletLinearTension(dirName, l1)
 

utils.py
@@ -444,7 +444,7 @@ def getStepList(numFrames, firstStep, stepFreq):
 def getDirectories(dirName):
     listDir = []
     for dir in os.listdir(dirName):
-        if(os.path.isdir(dirName + os.sep + dir) and (dir != "short" and dir != "dynamics")):
+        if(os.path.isdir(dirName + os.sep + dir) and (dir != "short" and dir != "augmented" and dir!="delaunayLabels")):
             listDir.append(dir)
     return listDir
 
@@ -452,7 +452,7 @@ def getOrderedDirectories(dirName):
     listDir = []
     listScalar = []
     for dir in os.listdir(dirName):
-        if(os.path.isdir(dirName + os.sep + dir) and (dir != "short" and dir != "dynamics")):
+        if(os.path.isdir(dirName + os.sep + dir) and (dir != "short" and dir != "augmented" and dir!="delaunayLabels")):
             listDir.append(dir)
             listScalar.append(dir.strip('t'))
     listScalar = np.array(listScalar, dtype=np.int64)

visuals.py
@@ -141,7 +141,7 @@ def plotSPPacking(dirName, figureName, ekmap=False, quiver=False, dense=False, b
     yBounds = np.array([0, boxSize[1]])
     #denseList = np.loadtxt(dirName + os.sep + "denseList.dat")
     pos = utils.centerPositions(pos, rad, boxSize)
-    #pos = utils.shiftPositions(pos, boxSize, 0.1, -0.3)
+    #pos = utils.shiftPositions(pos, boxSize, 0, 0.2)
     fig = plt.figure(0, dpi = 150)
     ax = fig.gca()
     ax.set_xlim(xBounds[0], xBounds[1])
@@ -291,7 +291,7 @@ def plotSPStressMapPacking(dirName, figureName, which='total', droplet=False, l1
     yBounds = np.array([0, boxSize[1]])
     rad = np.array(np.loadtxt(dirName + sep + "particleRad.dat"))
     pos = utils.getPBCPositions(dirName + os.sep + "particlePos.dat", boxSize)
-    pos = utils.shiftPositions(pos, boxSize, 0.2, -0.3)
+    pos = utils.shiftPositions(pos, boxSize, 0, -0.2)
     fig = plt.figure(0, dpi = 150)
     ax = fig.gca()
     ax.set_xlim(xBounds[0], xBounds[1])
@@ -378,6 +378,10 @@ def plotSPVoronoiPacking(dirName, figureName, dense=False, threshold=0.84, filte
     for i, cell in enumerate(cells):
         polygon = cell['vertices']
         ax.fill(*zip(*polygon), facecolor = 'none', edgecolor='k', lw=0.2)
+    plt.plot(pos[0,0], pos[0,1], marker='*', markersize=20, color='k')
+    plt.plot(pos[cells[0]['faces'][0]['adjacent_cell'],0], pos[cells[0]['faces'][0]['adjacent_cell'],1], marker='*', markersize=20, color='r')
+    plt.plot(pos[cells[0]['faces'][1]['adjacent_cell'],0], pos[cells[0]['faces'][1]['adjacent_cell'],1], marker='*', markersize=20, color='b')
+    plt.plot(pos[cells[0]['faces'][2]['adjacent_cell'],0], pos[cells[0]['faces'][2]['adjacent_cell'],1], marker='*', markersize=20, color='g')
     plt.tight_layout()
     figureName = "/home/francesco/Pictures/soft/packings/voronoi-" + figureName + ".png"
     plt.savefig(figureName, transparent=False, format = "png")
@@ -406,7 +410,7 @@ def plotSPDelaunayPacking(dirName, figureName, dense=False, threshold=0.78, filt
     yBounds = np.array([0, boxSize[1]])
     rad = np.array(np.loadtxt(dirName + sep + "particleRad.dat"))
     pos = utils.getPBCPositions(dirName + os.sep + "particlePos.dat", boxSize)
-    pos = utils.shiftPositions(pos, boxSize, 0, -0.2) # for 4k and 16k, -0.3, 0.1 for 8k
+    pos = utils.shiftPositions(pos, boxSize, 0, -0.2) # for 4k and 16k, -0.3, 0.1 for 8k 0 -0.2
     fig = plt.figure(0, dpi = 150)
     ax = fig.gca()
     ax.set_xlim(xBounds[0], xBounds[1])
@@ -416,7 +420,7 @@ def plotSPDelaunayPacking(dirName, figureName, dense=False, threshold=0.78, filt
     #setBigBoxAxes(boxSize, ax, 0.1)
     colorId = getRadColorList(rad)
     if(dense==True):
-        if(os.path.exists(dirName + os.sep + "delaunayList.dat")):
+        if(os.path.exists(dirName + os.sep + "delaunayList!.dat")):
             denseList = np.loadtxt(dirName + os.sep + "delaunayList.dat")
         else:
             denseList,_ = cluster.computeDelaunayCluster(dirName, threshold, filter=filter)
@@ -430,9 +434,9 @@ def plotSPDelaunayPacking(dirName, figureName, dense=False, threshold=0.78, filt
     if(colored == 'colored'):
         np.seterr(divide='ignore', invalid='ignore')
         newPos, simplices, colorId, simplexDensity = cluster.computeAugmentedDelaunayCluster(dirName, threshold, filter, 0, -0.2) # colorId is 0 for dense and 1 for dilute
-        plt.tripcolor(newPos[:,0], newPos[:,1], simplices[colorId==0], lw=0.3, facecolors=colorId[colorId==0], edgecolors='k', alpha=0.5, cmap='bwr')
+        plt.tripcolor(newPos[:,0], newPos[:,1], simplices, lw=0.3, facecolors=colorId, edgecolors='k', alpha=0.5, cmap='bwr')
         if(filter == 'filter'):
-            figureName = "/home/francesco/Pictures/soft/packings/fluidDelaunay-" + figureName + ".png"
+            figureName = "/home/francesco/Pictures/soft/packings/delaunay-" + figureName + ".png"
         else:
             figureName = "/home/francesco/Pictures/soft/packings/delaunay-" + figureName + ".png"
     else: